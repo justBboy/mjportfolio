0cc54c32cf38df2678ed8204e82ef61f4d4fffe4
Bboy <bornscience1@gmail.com>
2020-11-20 16:20:50 -0800

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'b$jrtg^k(3^ka9es97%mj+$e!jtre5*hgtv*!nk&s7#d^6_enb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
